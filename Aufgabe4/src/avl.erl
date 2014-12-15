52fe18a7c161429adcbbf901884d4ab99a23214f
xKrieger135 <patrick.steinhauer@haw-hamburg.de>
2014-12-15 16:05:05 +0100

    Last changes

--- Aufgabe4/src/avl.erl
+++ Aufgabe4/src/avl.erl
@@ -10,7 +10,7 @@
 -author("JanDennis").
 
 %% API
--export([create/0,add/2,delete/2]).
+-export([create/0,add/2,delete/2,eval/1]).
 
 create() -> {}.
 
@@ -23,15 +23,32 @@ add({Left, Current, Right}, Value) ->
 
 delete({}, _Value) -> {};
 delete({Left, Value, Right}, Value) ->
+  io:format("~p~n",["val found."]),
   if
     Left == {} -> Right;
     Right== {} -> Left;
-    true       ->  {}
+    true       ->  Smallest = getSmallest(Right),
+      {Left,Smallest,delete(Right,Smallest)}
   end;
 
 delete({Left, Any, Right}, Value) ->
-      Smallest = getSmallest(Right),
-      {Left,Smallest,delete(Right,Smallest)}.
+  io:format("Go deeper - ",[]),
+      if
+        Any<Value -> io:format("Right~n",[]),{Left, Any,delete(Right,Value)};
+        true -> io:format("Left~n",[]),{delete(Left,Value),Any,Right}
+      end.
 
 getSmallest({{},C,R}) -> C;
 getSmallest({L,C,R}) -> getSmallest(L).
+
+
+eval({}) -> io:format("Empty~n~n",[]);
+eval({{},Elem,{}}) -> io:format("Last Elem - ~p~n~n",[Elem]);
+eval({L,E,R}) ->
+  io:format("Top   - ~p~n",[E]),
+  io:format("Left  - ~p~n",[L]),
+  io:format("Right - ~p~n~n",[R]),
+  io:format("Next -> Left~n",[]),
+  eval(L),
+  io:format("Next -> Right~n",[]),
+  eval(R).
